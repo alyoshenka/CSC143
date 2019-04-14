b88a8e13489898138a0a6f7b7f2adcb54ac5060e
Alexi Most <alexibmost@gmail.com>
2019-04-14 12:03:31 -0700

    fixed gitignore

--- P2/Week02b/Animals/Grouper.java
+++
@@ -1,9 +0,0 @@
-public class Grouper extends Fish {
-
-    public static final String GROUPER_DIET = "animals";
-
-    public Grouper() {
-        super(GROUPER_DIET);
-    }
-    
-}
--- P2/Week02b/Animals/Panda.java
+++
@@ -1,7 +0,0 @@
-public class Panda extends Bear {
-
-    public Panda() {
-        super();
-    }
-    
-}
